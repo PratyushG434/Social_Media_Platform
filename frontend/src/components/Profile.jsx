0ee31c5d8fe182d2bebedac73d879a47d44d4754
abhaylodhi014 <abhaylodhi128135@gmail.com>
2025-11-17 15:07:14 +0530

    profile logout

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -304,7 +304,16 @@ export default function Profile() {
     setTargetUserForChat(user.user_id);
     navigate("/dashboard/messages");
   };
-
+   const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/login");
+    } catch (err) {
+      console.error("Logout failed", err);
+      await logout();
+      navigate("/login");
+    }
+  };
   // --- Like Toggle per Post ---
   const handleLikeToggle = (postId) => {
     setUserPosts((prevPosts) =>
@@ -506,9 +515,10 @@ export default function Profile() {
                   >
                     Edit Profile
                   </button>
-                  {/* <button className="bg-muted text-card-foreground py-2 px-4 rounded-lg font-medium hover:bg-muted/80 transition-colors">
-                    Share Profile
-                  </button> */}
+                  <button 
+                  onClick={handleLogout}  className="bg-muted text-card-foreground py-2 px-4 rounded-lg font-medium hover:bg-muted/80 transition-colors">
+                    logout
+                  </button>
                 </>
               ) : (
                 <>
