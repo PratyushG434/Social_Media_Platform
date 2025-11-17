9d266bd012da25eee6f8affcd76ce18050b0be38
PratyushG434 <pratyush1534@gmail.com>
2025-11-17 17:50:09 +0530

    Update CORS origin for backend application

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ app.use(
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://social-media-platform-ten-snowy.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://streamsocial.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Expose-Headers", "Set-Cookie");
   next();
